2fe3efa6fb1e1b190cf7d61b30975bb8eefdd764
Angelo Maiele <angelomaiele@gamil.com>
2024-10-07 15:19:06 +0200

    apuntes

---
+++ backend/controllers/apuntesController.js
@@ -0,0 +1,78 @@
+const asyncHanlder = require('express-async-handler')
+const Apuntes = require('../model/apuntesModel')
+
+const getSheetNotesByUserId = asyncHanlder(async(req, res) => {
+    const notesDelUsuario = await Apuntes.find({ userId: req.params.userId });
+
+    if (!notesDelUsuario || notesDelUsuario.length === 0) {
+        res.status(404);
+        throw new Error("No se encontraron notas para este usuario");
+    }
+
+    res.json(notesDelUsuario);
+})
+
+const createSheetNotes = asyncHanlder(async(req, res) => {
+    const {
+        preguntas, argumentos, conclusiones, evidencias, name, date, userId
+      } = req.body;
+    
+      console.log(req.body);
+
+    
+      if (!preguntas || !argumentos || !conclusiones || !evidencias || !userId ) {
+        res.status(400);
+        throw new Error("Please add all required fields");
+      }
+    
+      const nuevosApuntes = await Apuntes.create({
+        preguntas, argumentos, conclusiones, evidencias, userId
+      });
+    
+      if (nuevosApuntes) {
+        res.status(201).json({
+          _id: nuevosApuntes.id,
+          preguntas: nuevosApuntes.preguntas,
+          conclusiones: nuevosApuntes.conclusiones,
+          argumentos: nuevosApuntes.argumentos,
+          evidencias: nuevosApuntes.evidencias,
+          userId: nuevosApuntes.userId
+        });
+      } else {
+        res.status(400);
+        throw new Error("Meditation wasn't registered correctly");
+      }
+})
+
+const editSheetNotes = asyncHanlder(async(req, res) => {
+  const updatedApuntes = await Apuntes.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+});
+
+if (!updatedApuntes) {
+    res.status(404);
+    throw new Error("estos Apuntes no se encuentran");
+}
+
+res.status(200).json(updatedApuntes);
+})
+
+const deleteSheetNotes = asyncHanlder(async(req, res) => {
+  const deletedApuntes = await Apuntes.findByIdAndDelete(req.params.id);
+
+  if (!deletedApuntes) {
+      res.status(404);
+      throw new Error("estos Apuntes no se encuentran");
+  }
+
+  res.status(200).json({ message: `Apuntes con el id ${req.params.id} deleted successfully` });
+})
+
+
+
+module.exports = {
+  getSheetNotesByUserId,
+  createSheetNotes,
+  editSheetNotes,
+  deleteSheetNotes,
+}
--- backend/server.js
+++ backend/server.js
@@ -139,6 +139,7 @@ app.use('/api/pomodoro', require("./routes/pomodoroRoutes"))
 app.use('/api/checkout', require("./routes/checkoutRoutes"))
 app.use('/api/motivationalNotes', require("./routes/notesRoutes"))
 app.use('/api/resume', require("./routes/resumeRoutes"))
+app.use('/api/apuntes', require("./routes/apuntesRoutes"))
 
 app.use(errorHandler)
 
