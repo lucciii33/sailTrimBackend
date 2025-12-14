9d62e3b085b5c891a25208ca0e8cd49ffa133ddc
Angelo Maiele <angelomaiele@gamil.com>
2025-12-14 19:40:12 +0000

    rr

---
+++ backend/controllers/maintenanceController.js
@@ -0,0 +1,6 @@
+const MaintenanceLog = require("../model/MaintenanceLogModel");
+const asyncHandler = require("express-async-handler");
+
+const createMaintenance = asyncHandler(async (req, res) => {});
+
+module.exports = {};
