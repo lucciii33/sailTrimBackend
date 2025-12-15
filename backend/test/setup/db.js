a862a227214e26497f37ba685f61ec87974911fc
Angelo Maiele <angelomaiele@gamil.com>
2025-12-15 19:04:10 +0000

    fixing pipeline

--- backend/test/setup/db.js
+++ backend/test/setup/db.js
@@ -3,3 +3,10 @@ const mongoose = require("mongoose");
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URL);
 });
+
+afterAll(async () => {
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.connection.close();
+    console.log("Mongoose connection closed after all tests.");
+  }
+});
